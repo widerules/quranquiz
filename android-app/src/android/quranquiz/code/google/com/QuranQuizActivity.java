257404e170bd0d3baf2069419f1bac7f4fd2b5e7
tarekeldeeb <tarekeldeeb@gmail.com>
2011-12-12 05:34:49 +0000

    added a trial goupBotton and arabic font textView

---
+++ android-app/src/android/quranquiz/code/google/com/QuranQuizActivity.java
@@ -0,0 +1,67 @@
+package android.quranquiz.code.google.com;
+
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class QuranQuizActivity extends Activity implements RadioGroup.OnCheckedChangeListener {
+
+	private TextView tv;
+	private RadioGroup rgQQOptions;
+	private int i=0;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		tv = (TextView) findViewById(R.id.textView1);
+		Typeface othmanyFont = Typeface.createFromAsset(getAssets(), "fonts/KacstQurn.ttf");
+		tv.setTypeface(othmanyFont);
+		
+		rgQQOptions = (RadioGroup) findViewById(R.id.radioQQOptions);
+		rgQQOptions.setOnCheckedChangeListener(this);
+		
+	}
+
+	public void onCheckedChanged(RadioGroup rg, int CheckedID) {
+		int SelID=-2;
+
+		switch(CheckedID){
+		case R.id.radioOp1:
+			SelID=0;
+			break;
+		case R.id.radioOp2:
+			SelID=1;
+			break;
+		case R.id.radioOp3:
+			SelID=2;
+			break;
+		case R.id.radioOp4:
+			SelID=3;
+			break;
+		case R.id.radioOp5:
+			SelID=4;
+			break;
+		}
+		if( SelID<0)
+			return;
+		
+		userAction(SelID);
+		//rgQQOptions.clearCheck();
+		((RadioButton)rgQQOptions.getChildAt(SelID)).setChecked(false);
+	}
+
+private void userAction(int selID) {
+	i = i+selID+1;
+	String strTemp = new String();
+	for(int j=0;j<5;j++){
+		strTemp = new Integer(i+j).toString();
+		((RadioButton)rgQQOptions.getChildAt(j)).setText(strTemp);
+	}
+	
+}
+}
