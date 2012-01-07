d66b07c7ce0e946cb8992bc283fc2fb02cb5c459
tarekeldeeb <tarekeldeeb@gmail.com>
2012-01-07 11:43:56 +0000

    compiles, has many exceptions, doesnt start, code porting complete, buggy

---
+++ android-app/src/com/google/code/quranquiz/QQQuestion.java
@@ -0,0 +1,161 @@
+package com.google.code.quranquiz;
+
+import java.util.List;
+import java.util.Random;
+
+public class QQQuestion {
+	public int[][] op = new int[10][5];	// Holds all options for the current Question
+	public int startIdx,validCount;		// Precise Position near seed, valid options
+	public int qLen;					// Length of the Question
+	
+	private int lastSeed;				// Seed for the Question
+	private int level;					// User Level, currently
+	private QQDataBaseHelper q;			// Reference to the DB
+	private Random rand;				// Random Generator for the next Question
+	
+	public QQQuestion(int previousSeed, int userlevel, QQDataBaseHelper qdb){
+		
+		// If no seed, -ve, in case of a newly created profile
+		// Then generate a new random starting point
+		if(previousSeed < 0)
+			rand = new Random();
+		else // resume from the last seed
+			rand = new Random(previousSeed);
+		
+		// Keep Reference of Level, Q
+		level = userlevel;
+		q = qdb;
+		createQ();
+
+	}
+	
+	private void createQ() {
+		lastSeed = rand.nextInt(77797);
+		
+		// +1 to compensate the rand-gen integer [0-77796]
+		startIdx = getValidStartNear(lastSeed+1); 
+		
+		fillCorrectOptions();
+		
+		fillIncorrectOptions();
+		
+	}	
+
+	private int getValidStartNear(int start) {
+	    // Search for a correct neighbor start according to level
+	    int dir=1; // search down = +1
+	    int limitHit=1,disp2,disp3;
+	    int start_shadow;
+	    boolean srch_cond;
+	    while (limitHit>0){
+	        start_shadow = start;
+	        limitHit=0;
+	        srch_cond = true;
+	        while (srch_cond){
+	            start_shadow = start_shadow+dir;
+	            if (start_shadow == 0 || start_shadow == 77798){
+	                limitHit=1;
+	                dir = -dir;
+	                break;
+	            }
+	            if (level==1){ // Get a non-motashabehat near selected index
+	                // Motashabehat found,continue!
+	                srch_cond=q.sim2cnt(start_shadow) >1; //TODO: check: Is 0 better? 
+	                validCount = 1;	// \
+	                qLen   = 3;		// -|-> Default Constants for level-1
+	            } else {
+	                // Search for a motashabehat near selected index
+	                // Specify # Words to display
+	                disp2=0;disp3=0;  
+	                
+	                if(q.sim3cnt(start_shadow)<5 && q.sim3cnt(start_shadow)>0)
+	                    disp3=q.sim3cnt(start_shadow);
+	                
+	                if(q.sim2cnt(start_shadow) <5 && q.sim2cnt(start_shadow)>0)
+	                    disp2=q.sim2cnt(start_shadow);
+	                
+	                // Motashabehat not found,continue!            
+	                srch_cond = (disp3==0  && disp2==0);
+	                
+	                if (srch_cond == false){ // Found!
+	                	validCount 	= (disp2>disp3)? disp2+1: disp3+1;// Count start-position as well
+	                	qLen		= (disp2>disp3)? 1:2;
+	                }
+	            } 
+	    	}
+
+	        start = start_shadow;       
+	    }
+	    return start;
+	}
+	
+	private void fillCorrectOptions() {
+	    // fill Correct Option Words @indx=1 (2,3,..validCount for higher levels)
+		List<Integer> tmp;
+	    op[0][0]=startIdx+qLen;
+	    for(int k=1;k<10;k++){
+	    	op[k][0] = op[k-1][0]+1;
+	    }
+	    if (level>1){
+	        if (qLen==1){ // A 2-word Question
+	        	tmp = q.sim2idx(startIdx);
+	        	for(int i=1;i<validCount;i++)
+	        		op[0][i] = tmp.get(i);
+	        }
+	        else{  // A 3-word Question
+	        	tmp = q.sim3idx(startIdx);
+	        	for(int i=1;i<validCount;i++)
+	        		op[0][i] = tmp.get(i);
+	        	}
+    	    for(int k=1;k<10;k++)
+    	    	for(int j=1;j<validCount;j++)
+    	    		op[k][j]=op[k-1][j]+1;
+	    }		
+	}
+	
+	private void fillIncorrectOptions() {    
+	    // Get Valid Options @indx=2:5 (validCount+1:5 for higher levels)
+	    // Get the next words to similar-to-previous  
+	    // Get unique options
+		int last_correct,uniq_cnt;
+		List<Integer> diffList;
+		
+	    for(int i=0;i<10;i++){
+	        last_correct = op[i][0]-1;
+	        
+	        // We want to remove redundant correct choices from the given
+	        // options, this is made by removing subset sim2 from sim1
+	        diffList =q.sim1idx(last_correct);
+	        diffList.removeAll(q.sim2idx(last_correct));
+	        
+	        // '+1' means the next word
+	        diffList = q.uniqueWordsList(diffList);
+	        diffList = QQUtils.ListPlus(diffList,1);
+	        uniq_cnt= diffList.size();
+	        
+	        int[] rnd_idx = new int[uniq_cnt];
+	        Random randg = new Random();
+	        
+	        if(uniq_cnt > 3){
+	            rnd_idx = QQUtils.randperm(uniq_cnt);
+	            for(int j=1;j<5;j++){
+	            	op[i][j] = diffList.get(rnd_idx[j]);
+	            }
+	        }
+	        else if (uniq_cnt > 0){
+	            rnd_idx = QQUtils.randperm(uniq_cnt);
+	            for(int j=1;j<uniq_cnt+1;j++){
+	            	op[i][j] = diffList.get(rnd_idx[j]);
+	            }
+	            for(int j=uniq_cnt+1;j<5;j++){
+	            	op[i][j] = randg.nextInt(77799);
+	            }
+	        }
+	        else{ // uniq_cnt=0, all random options!
+	        	for(int j=1;j<5;j++)
+	            	op[i][j] = randg.nextInt(77799);
+	        }
+	    }		
+	}
+
+}
--- android-app/src/com/google/code/quranquiz/QuranQuizActivity.java
+++ android-app/src/com/google/code/quranquiz/QuranQuizActivity.java
@@ -11,35 +11,20 @@ import com.google.code.quranquiz.R;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class QuranQuizActivity extends Activity implements RadioGroup.OnCheckedChangeListener {
 
 	private TextView tv;
 	private RadioGroup rgQQOptions;
 	private QQDataBaseHelper q;
-	private int i=0;
-
+	private QQQuestion Quest;
+	private int QOptIdx=-1;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		Typeface othmanyFont = Typeface.createFromAsset(getAssets(), "fonts/KacstQurn.ttf");
-		tv = (TextView) findViewById(R.id.textView1);
-		tv.setTypeface(othmanyFont);
-		tv = (TextView) findViewById(R.id.radioOp1);
-		tv.setTypeface(othmanyFont);
-		tv = (TextView) findViewById(R.id.radioOp2);
-		tv.setTypeface(othmanyFont);
-		tv = (TextView) findViewById(R.id.radioOp3);
-		tv.setTypeface(othmanyFont);
-		tv = (TextView) findViewById(R.id.radioOp4);
-		tv.setTypeface(othmanyFont);
-		tv = (TextView) findViewById(R.id.radioOp5);
-		tv.setTypeface(othmanyFont);
-		
-		rgQQOptions = (RadioGroup) findViewById(R.id.radioQQOptions);
-		rgQQOptions.setOnCheckedChangeListener(this);
-		
 		
         q = new QQDataBaseHelper(this);
         try {
@@ -54,6 +39,29 @@ public class QuranQuizActivity extends Activity implements RadioGroup.OnCheckedC
 	 	} catch(SQLException sqle) {
 	 		throw sqle;
 	 	}
+	 	
+		Typeface othmanyFont = Typeface.createFromAsset(getAssets(), "fonts/KacstQurn.ttf");
+		tv = (TextView) findViewById(R.id.textView1);
+		tv.setTypeface(othmanyFont);
+		tv.setText(q.txt(5, 3));
+		tv = (TextView) findViewById(R.id.radioOp1);
+		tv.setTypeface(othmanyFont);
+		tv = (TextView) findViewById(R.id.radioOp2);
+		tv.setTypeface(othmanyFont);
+		tv = (TextView) findViewById(R.id.radioOp3);
+		tv.setTypeface(othmanyFont);
+		tv = (TextView) findViewById(R.id.radioOp4);
+		tv.setTypeface(othmanyFont);
+		tv = (TextView) findViewById(R.id.radioOp5);
+		tv.setTypeface(othmanyFont);
+		
+		tv = (TextView) findViewById(R.id.textView1);
+
+		rgQQOptions = (RadioGroup) findViewById(R.id.radioQQOptions);
+		rgQQOptions.setOnCheckedChangeListener(this);
+		
+		
+
 	}
 
 	@Override
@@ -91,12 +99,50 @@ public class QuranQuizActivity extends Activity implements RadioGroup.OnCheckedC
 	}
 
 private void userAction(int selID) {
-	i = i+selID+1;
+	//TODO: Grab the last seed from the loaded profile! (replace -1, level 1)
+	int level = 1;
+	int lastSeed = -1;
+	
+	if(QOptIdx == -1 || QOptIdx == 10){
+		Quest = new QQQuestion(lastSeed,level,q); 
+		
+		// Show the Question!
+		tv.setText(tv.getText().toString().concat(q.txt(Quest.startIdx,Quest.qLen)));
+		QOptIdx = 0;
+	}
+	
+	// Concat correct options to the Question!
+	if(QOptIdx>0)
+		tv.setText(tv.getText().toString().concat(q.txt(Quest.startIdx+Quest.qLen+QOptIdx-1)+ ' '));
+	
+    //Scramble options
+    int[] scrambled = new int[5];
+    scrambled  = QQUtils.randperm(5);
+    int correct_choice = QQUtils.findIdx(scrambled,1); //idx=1
+    
+    //Display Options:
 	String strTemp = new String();
 	for(int j=0;j<5;j++){
-		strTemp = q.txt(i+j);
+		strTemp = q.txt(Quest.op[QOptIdx][j]) ;
 		((RadioButton)rgQQOptions.getChildAt(j)).setText(strTemp);
 	}
 	
+    if(level==2){
+        //display(['    -- ',num2str(validCount),' correct options left!']); % TODO: Subtract done options
+    }
+    else if(level==3 && QOptIdx==1){
+        //display('  [-] No more valid Motashabehat!');
+    }
+    
+
+        // Check if wrong choice
+        if(correct_choice != selID){
+            //Display Correct answer
+    		Toast.makeText(this, q.txt(Quest.startIdx,10), Toast.LENGTH_LONG).show();
+            QOptIdx = -1;
+        }
+
+
+	QOptIdx = QOptIdx +1;
 }
 }
