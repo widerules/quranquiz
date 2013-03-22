5c47ed7d7b46a3ae6ca41d38a23ff41d606de145
tarekeldeeb <tarekeldeeb@gmail.com>
2013-03-22 14:35:51 +0000

    toggle all added to top/bottom of the studyParts List

--- android-app/src/com/google/code/quranquiz/QQStudyListActivity.java
+++ android-app/src/com/google/code/quranquiz/QQStudyListActivity.java
@@ -2,13 +2,20 @@ package com.google.code.quranquiz;
 
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceCategory;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ToggleButton;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
-public class QQStudyListActivity extends SherlockPreferenceActivity {
+public class QQStudyListActivity extends SherlockPreferenceActivity{
 
+	private Button toggleSelection;
 	private QQProfileHandler ProfileHandler;
+	private PreferenceCategory targetCategory;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +26,25 @@ public class QQStudyListActivity extends SherlockPreferenceActivity {
 		// add the layout
 		addPreferencesFromResource(R.xml.study_list);
 
+		((Preference)findPreference("toggle_button_top")).
+			setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+		                public boolean onPreferenceClick(Preference arg0) { 
+		                	ToggleAll();
+		                	return true;
+		                }
+		            });
+		
+		((Preference)findPreference("toggle_button_bottom")).
+		setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+	                public boolean onPreferenceClick(Preference arg0) { 
+	                	ToggleAll();
+	                	return true;
+	                }
+	            });        
+        
 		// fetch the item where you wish to insert the CheckBoxPreference, in
 		// this case a PreferenceCategory with key "targetCategory"
-		PreferenceCategory targetCategory = (PreferenceCategory) findPreference("pref_studyList");
+		targetCategory = (PreferenceCategory) findPreference("pref_studyList");
 
 		// Get the passed QQProfile
 		ProfileHandler = (QQProfileHandler) getIntent().getSerializableExtra(
@@ -33,11 +56,12 @@ public class QQStudyListActivity extends SherlockPreferenceActivity {
 			// make sure each key is unique
 			checkBoxPreference.setKey("QPart_s" + String.valueOf(i + 1));
 			checkBoxPreference.setTitle(" سورة " + QQUtils.sura_name[i]);
-			checkBoxPreference.setSummary(String
-					.valueOf(ProfileHandler.CurrentProfile.getCorrect(i))
+			checkBoxPreference.setSummary( " إجاباتك الصحيحة "
+					+ String.valueOf(ProfileHandler.CurrentProfile.getCorrect(i))
 					+ " من "
 					+ String.valueOf(ProfileHandler.CurrentProfile
-							.getQuesCount(i))); // TODO: Prev score
+							.getQuesCount(i))
+					); 
 			if (i == 0) {
 				checkBoxPreference.setEnabled(false);
 				checkBoxPreference.setChecked(true);
@@ -48,8 +72,8 @@ public class QQStudyListActivity extends SherlockPreferenceActivity {
 			checkBoxPreference = new CheckBoxPreference(this);
 			checkBoxPreference.setKey("QPart_j" + String.valueOf(i + 26));
 			checkBoxPreference.setTitle(" جزء " + QQUtils.last5_juz_name[i]);
-			checkBoxPreference.setSummary(String
-					.valueOf(ProfileHandler.CurrentProfile.getCorrect(45 + i))
+			checkBoxPreference.setSummary( " إجاباتك الصحيحة "
+					+ String.valueOf(ProfileHandler.CurrentProfile.getCorrect(45 + i))
 					+ " من "
 					+ String.valueOf(ProfileHandler.CurrentProfile
 							.getQuesCount(45 + i))); // TODO: Prev score
@@ -59,6 +83,13 @@ public class QQStudyListActivity extends SherlockPreferenceActivity {
 			}
 			targetCategory.addPreference(checkBoxPreference);
 		}
+	}
+
+	public void ToggleAll() {
+    	Boolean toggle = !((CheckBoxPreference)targetCategory.getPreference(2)).isChecked();
+    	for (int i=1;i<targetCategory.getPreferenceCount()-1;i++){
+    		((CheckBoxPreference)targetCategory.getPreference(i)).setChecked(toggle);
+    	}
 	}
 
 	@Override
