14a21884d41a3d5a3e9455866a2d3cb044e08887
tarekeldeeb@gmail.com <tarekeldeeb@gmail.com@72042f65-f49f-be1f-1ff3-66322bd4e578>
2013-04-19 15:06:07 +0000

    added an instruction splash scrreen. Always show, it should only appear at the first use only.

---
+++ trunk/android-app/src/net/quranquiz/QQInstructionsActivity.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package net.quranquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class QQInstructionsActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.instructions_layout);
+		ImageView instructions = (ImageView) findViewById(R.id.ivInstructions);
+		instructions.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				finish();
+		    }
+		});
+	}
+}
