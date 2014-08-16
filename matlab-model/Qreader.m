2e4cc70f4e807419eb6a447b652af5b5738e4290
tarekeldeeb <tarekeldeeb@gmail.com>
2014-08-16 23:30:13 +0000

    Generated the simn field

--- matlab-model/Qreader.m
+++ matlab-model/Qreader.m
@@ -1,8 +1,9 @@
 cd % Parses the Quran Words ..
 clc;clear;
 
-words_cnt = 77878;
 txt_sym=2;
+gendoc = 1;
+words_cnt = 77878;
 fid=fopen('quran-uthmani-min.nosym.txt');
 fid2=fopen('quran-uthmani-min.sym.txt');
 aya=1;
@@ -30,7 +31,7 @@ fclose(fid);
 fclose(fid2);
 
 display('Parsing .. (%)');
-last_p = -1;
+last_p = -1; lskip = 0;
 for i=1:words_cnt-2
     tmp = find(strcmp(q.txt(:),q.txt(i))>0);
     q.sim1(i).cnt = length(tmp);
@@ -42,27 +43,50 @@ for i=1:words_cnt-2
     q.sim3(i).cnt = length(q.sim3(i).idx);
     
     %Longer patterns are applicable!
+    if(gendoc==1 && q.sim3(i).cnt>1  && i<words_cnt-4)
+            n=2;
+            sim =  q.sim3(i).idx;
+            while(~isempty(sim))
+                n=n+1;
+                sim_shadow = sim;
+                sim = sim(strcmp(q.txt(sim(sim<(words_cnt-n+1))+n),q.txt(i+n))>0);
+            end
+            if( i==1)  % Initial condition for proper indexing
+                q.simn(i).idx=sim_shadow';
+                q.simn(i).cnt=n;
+                lskip = 1;
+            elseif(q.simn(i-lskip).cnt  ~= n+lskip )
+                q.simn(i).idx=sim_shadow';
+                q.simn(i).cnt=n;
+                lskip = 1;
+            else
+                lskip = lskip +1;
+                q.simn(i).cnt=0;
+            end
+    else
+        q.simn(i).cnt=0;
+    end
+    
     if(round(i/words_cnt*100)>last_p)
         display(num2str(round(i/words_cnt*100)));
         last_p = round(i/words_cnt*100);
     end
 end
 % tailed loops
-    tmp = find(strcmp(q.txt(:),q.txt(words_cnt-1))>0);
-    q.sim1(words_cnt-1).cnt = length(tmp);
-    q.sim1(words_cnt-1).idx = tmp(tmp~=words_cnt-1);
-    q.sim2(words_cnt-1).idx = q.sim1(words_cnt-1).idx(strcmp(q.txt(q.sim1(words_cnt-1).idx(q.sim1(words_cnt-1).idx<words_cnt)+1)    ,q.txt(words_cnt-1+1))>0);
-    q.sim2(words_cnt-1).cnt = length(q.sim2(words_cnt-1).idx);
-    
-    tmp = find(strcmp(q.txt(:),q.txt(words_cnt))>0);
-    q.sim1(words_cnt).cnt = length(tmp);
-    q.sim1(words_cnt).idx = tmp(tmp~=words_cnt);
-    
-    
+tmp = find(strcmp(q.txt(:),q.txt(words_cnt-1))>0);
+q.sim1(words_cnt-1).cnt = length(tmp);
+q.sim1(words_cnt-1).idx = tmp(tmp~=words_cnt-1);
+q.sim2(words_cnt-1).idx = q.sim1(words_cnt-1).idx(strcmp(q.txt(q.sim1(words_cnt-1).idx(q.sim1(words_cnt-1).idx<words_cnt)+1)    ,q.txt(words_cnt-1+1))>0);
+q.sim2(words_cnt-1).cnt = length(q.sim2(words_cnt-1).idx);
+
+tmp = find(strcmp(q.txt(:),q.txt(words_cnt))>0);
+q.sim1(words_cnt).cnt = length(tmp);
+q.sim1(words_cnt).idx = tmp(tmp~=words_cnt);
+
 save QWords.mat q;
 
- fido=fopen('idx-data.csv','w');
- 
+fido=fopen('idx-data.csv','w');
+
  if(txt_sym==0)
     for i=1:words_cnt-2
         fprintf(fido,'%d,%s,%d,%d,%d\n', i, char(q.txt(i)),q.sim1(i).cnt,q.sim2(i).cnt,q.sim3(i).cnt);
@@ -77,11 +101,12 @@ save QWords.mat q;
     fprintf(fido,'%d,%s,%d,0,0\n', words_cnt, char(q.txt_sym(words_cnt)),q.sim1(words_cnt).cnt);
  else %Print both sym and noSym
     for i=1:words_cnt-2
-        fprintf(fido,'%d,%s,%s,%d,%d,%d,%s\n', i, char(q.txt(i)),char(q.txt_sym(i)),q.sim1(i).cnt,q.sim2(i).cnt,q.sim3(i).cnt,char(q.aya(i)));
+        fprintf(fido,'%d,%s,%s,%d,%d,%d,%s,%d\n', i, char(q.txt(i)),char(q.txt_sym(i)),q.sim1(i).cnt,q.sim2(i).cnt,q.sim3(i).cnt,char(q.aya(i)),q.simn(i).cnt);
     end
-    fprintf(fido,'%d,%s,%s,%d,%d,0,%s\n', words_cnt-1, char(q.txt(words_cnt-1)),char(q.txt_sym(words_cnt-1)),q.sim1(words_cnt-1).cnt,q.sim2(words_cnt-1).cnt,char(q.aya(words_cnt-1)));
-    fprintf(fido,'%d,%s,%s,%d,0,0,%s\n', words_cnt, char(q.txt(words_cnt)),char(q.txt_sym(words_cnt)),q.sim1(words_cnt).cnt,char(q.aya(words_cnt)));     
+    fprintf(fido,'%d,%s,%s,%d,%d,0,%s,0\n', words_cnt-1, char(q.txt(words_cnt-1)),char(q.txt_sym(words_cnt-1)),q.sim1(words_cnt-1).cnt,q.sim2(words_cnt-1).cnt,char(q.aya(words_cnt-1)));
+    fprintf(fido,'%d,%s,%s,%d,0,0,%s,0\n', words_cnt, char(q.txt(words_cnt)),char(q.txt_sym(words_cnt)),q.sim1(words_cnt).cnt,char(q.aya(words_cnt)));     
  end
  
  fclose(fido);
 display('Done!');
+
